e4fde5778d10a3e44434f310cba10b2c459df5cb
taukir4u <taukir4u@gmail.com>
2014-12-26 18:43:45 +0600

    C#
    Signed-off-by: taukir4u <taukir4u@gmail.com>

---
+++ AboutMyself/Form1.Designer.cs
@@ -0,0 +1,265 @@
+﻿namespace AboutMyself
+{
+    partial class AboutMyselfUI
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.firstNameTextBox = new System.Windows.Forms.TextBox();
+            this.lastNameTextBox = new System.Windows.Forms.TextBox();
+            this.fathersNameTextBox = new System.Windows.Forms.TextBox();
+            this.mothersNameTextBox = new System.Windows.Forms.TextBox();
+            this.addressTextBox = new System.Windows.Forms.TextBox();
+            this.saveButton = new System.Windows.Forms.Button();
+            this.showAllInfoButton = new System.Windows.Forms.Button();
+            this.showNameButton = new System.Windows.Forms.Button();
+            this.showParentsNameButton = new System.Windows.Forms.Button();
+            this.showAddressButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Cooper Black", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Indigo;
+            this.label1.Location = new System.Drawing.Point(126, 16);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(215, 19);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "My Personal Information";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Cooper Black", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.DarkSlateGray;
+            this.label2.Location = new System.Drawing.Point(30, 51);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(70, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "First Name";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Cooper Black", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.DarkSlateGray;
+            this.label3.Location = new System.Drawing.Point(36, 90);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(65, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Last Name";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Cooper Black", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.DarkSlateGray;
+            this.label4.Location = new System.Drawing.Point(15, 124);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(87, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Father\'s Name";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Cooper Black", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.Color.DarkSlateGray;
+            this.label5.Location = new System.Drawing.Point(12, 166);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(91, 13);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Mother\'s Name";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Cooper Black", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.ForeColor = System.Drawing.Color.DarkSlateGray;
+            this.label6.Location = new System.Drawing.Point(51, 205);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(52, 13);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Address";
+            // 
+            // firstNameTextBox
+            // 
+            this.firstNameTextBox.BackColor = System.Drawing.SystemColors.Info;
+            this.firstNameTextBox.ForeColor = System.Drawing.SystemColors.WindowText;
+            this.firstNameTextBox.Location = new System.Drawing.Point(107, 47);
+            this.firstNameTextBox.Name = "firstNameTextBox";
+            this.firstNameTextBox.Size = new System.Drawing.Size(273, 20);
+            this.firstNameTextBox.TabIndex = 6;
+            // 
+            // lastNameTextBox
+            // 
+            this.lastNameTextBox.BackColor = System.Drawing.SystemColors.Info;
+            this.lastNameTextBox.Location = new System.Drawing.Point(107, 87);
+            this.lastNameTextBox.Name = "lastNameTextBox";
+            this.lastNameTextBox.Size = new System.Drawing.Size(273, 20);
+            this.lastNameTextBox.TabIndex = 7;
+            // 
+            // fathersNameTextBox
+            // 
+            this.fathersNameTextBox.BackColor = System.Drawing.SystemColors.Info;
+            this.fathersNameTextBox.Location = new System.Drawing.Point(108, 124);
+            this.fathersNameTextBox.Name = "fathersNameTextBox";
+            this.fathersNameTextBox.Size = new System.Drawing.Size(273, 20);
+            this.fathersNameTextBox.TabIndex = 8;
+            // 
+            // mothersNameTextBox
+            // 
+            this.mothersNameTextBox.BackColor = System.Drawing.SystemColors.Info;
+            this.mothersNameTextBox.Location = new System.Drawing.Point(107, 163);
+            this.mothersNameTextBox.Name = "mothersNameTextBox";
+            this.mothersNameTextBox.Size = new System.Drawing.Size(273, 20);
+            this.mothersNameTextBox.TabIndex = 9;
+            // 
+            // addressTextBox
+            // 
+            this.addressTextBox.BackColor = System.Drawing.SystemColors.Info;
+            this.addressTextBox.Location = new System.Drawing.Point(107, 205);
+            this.addressTextBox.Multiline = true;
+            this.addressTextBox.Name = "addressTextBox";
+            this.addressTextBox.Size = new System.Drawing.Size(273, 44);
+            this.addressTextBox.TabIndex = 10;
+            // 
+            // saveButton
+            // 
+            this.saveButton.Font = new System.Drawing.Font("Cooper Black", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.saveButton.ForeColor = System.Drawing.Color.DarkOliveGreen;
+            this.saveButton.Location = new System.Drawing.Point(108, 279);
+            this.saveButton.Name = "saveButton";
+            this.saveButton.Size = new System.Drawing.Size(103, 23);
+            this.saveButton.TabIndex = 11;
+            this.saveButton.Text = "Save";
+            this.saveButton.UseVisualStyleBackColor = true;
+            this.saveButton.Click += new System.EventHandler(this.saveButton_Click);
+            // 
+            // showAllInfoButton
+            // 
+            this.showAllInfoButton.Font = new System.Drawing.Font("Cooper Black", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.showAllInfoButton.ForeColor = System.Drawing.Color.DarkOliveGreen;
+            this.showAllInfoButton.Location = new System.Drawing.Point(217, 279);
+            this.showAllInfoButton.Name = "showAllInfoButton";
+            this.showAllInfoButton.Size = new System.Drawing.Size(164, 23);
+            this.showAllInfoButton.TabIndex = 12;
+            this.showAllInfoButton.Text = "Show All Information";
+            this.showAllInfoButton.UseVisualStyleBackColor = true;
+            this.showAllInfoButton.Click += new System.EventHandler(this.showAllInfoButton_Click);
+            // 
+            // showNameButton
+            // 
+            this.showNameButton.Font = new System.Drawing.Font("Cooper Black", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.showNameButton.ForeColor = System.Drawing.Color.DarkOliveGreen;
+            this.showNameButton.Location = new System.Drawing.Point(108, 308);
+            this.showNameButton.Name = "showNameButton";
+            this.showNameButton.Size = new System.Drawing.Size(86, 23);
+            this.showNameButton.TabIndex = 13;
+            this.showNameButton.Text = "Name";
+            this.showNameButton.UseVisualStyleBackColor = true;
+            this.showNameButton.Click += new System.EventHandler(this.showNameButton_Click);
+            // 
+            // showParentsNameButton
+            // 
+            this.showParentsNameButton.Font = new System.Drawing.Font("Cooper Black", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.showParentsNameButton.ForeColor = System.Drawing.Color.DarkOliveGreen;
+            this.showParentsNameButton.Location = new System.Drawing.Point(200, 308);
+            this.showParentsNameButton.Name = "showParentsNameButton";
+            this.showParentsNameButton.Size = new System.Drawing.Size(97, 23);
+            this.showParentsNameButton.TabIndex = 14;
+            this.showParentsNameButton.Text = "Parents Name";
+            this.showParentsNameButton.UseVisualStyleBackColor = true;
+            this.showParentsNameButton.Click += new System.EventHandler(this.showParentsNameButton_Click);
+            // 
+            // showAddressButton
+            // 
+            this.showAddressButton.Font = new System.Drawing.Font("Cooper Black", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.showAddressButton.ForeColor = System.Drawing.Color.DarkOliveGreen;
+            this.showAddressButton.Location = new System.Drawing.Point(303, 308);
+            this.showAddressButton.Name = "showAddressButton";
+            this.showAddressButton.Size = new System.Drawing.Size(77, 23);
+            this.showAddressButton.TabIndex = 15;
+            this.showAddressButton.Text = "Address";
+            this.showAddressButton.UseVisualStyleBackColor = true;
+            this.showAddressButton.Click += new System.EventHandler(this.showAddressButton_Click);
+            // 
+            // AboutMyselfUI
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(431, 342);
+            this.Controls.Add(this.showAddressButton);
+            this.Controls.Add(this.showParentsNameButton);
+            this.Controls.Add(this.showNameButton);
+            this.Controls.Add(this.showAllInfoButton);
+            this.Controls.Add(this.saveButton);
+            this.Controls.Add(this.addressTextBox);
+            this.Controls.Add(this.mothersNameTextBox);
+            this.Controls.Add(this.fathersNameTextBox);
+            this.Controls.Add(this.lastNameTextBox);
+            this.Controls.Add(this.firstNameTextBox);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "AboutMyselfUI";
+            this.Text = "About Myself";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox firstNameTextBox;
+        private System.Windows.Forms.TextBox lastNameTextBox;
+        private System.Windows.Forms.TextBox fathersNameTextBox;
+        private System.Windows.Forms.TextBox mothersNameTextBox;
+        private System.Windows.Forms.TextBox addressTextBox;
+        private System.Windows.Forms.Button saveButton;
+        private System.Windows.Forms.Button showAllInfoButton;
+        private System.Windows.Forms.Button showNameButton;
+        private System.Windows.Forms.Button showParentsNameButton;
+        private System.Windows.Forms.Button showAddressButton;
+    }
+}
+
